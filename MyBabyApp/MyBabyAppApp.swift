6d41adf82bb001797744a1ef97c5843e064c5574
wannabemountainking <kdy0096@gmail.com>
2026-06-28 11:00:58 +0900

    Initial Commit

---
+++ MyBabyApp/MyBabyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBabyAppApp.swift
+//  MyBabyApp
+//
+//  Created by YoonieMac on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct MyBabyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
