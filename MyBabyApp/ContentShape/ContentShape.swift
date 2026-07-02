93ddbeecb84774bcb2c385f08a05484ee8f3b55a
wannabemountainking <kdy0096@gmail.com>
2026-07-02 10:36:09 +0900

    lastTextBaseline

--- MyBabyApp/ContentShape/ContentShape.swift
+++ MyBabyApp/ContentShape/ContentShape.swift
@@ -9,19 +9,32 @@ import SwiftUI
 
 struct ContentShape: View {
     var body: some View {
-		Button(
-			"write",
-			systemImage: "pencil.and.outline",
-			action: { print("작동")}
-		)
-		.frame(width: 24, height: 24)
-		.foregroundStyle(Gradient(colors: [.pink, .green]))
-		.background(Color.yellow)
+//		Button(
+//			"write",
+//			systemImage: "pencil.and.outline",
+//			action: { print("작동")}
+//		)
+//		.frame(width: 24, height: 24)
+//		.foregroundStyle(Gradient(colors: [.pink, .green]))
+//		.background(Color.yellow)
+//		.padding(20)
+//		.contentShape(
+//			Rectangle()
+//		)
+//		.buttonStyle(.plain)
+		
+		HStack {
+			Text("설정")
+			Spacer()
+			Image(systemName: "chevron.right")
+		}
+		.frame(maxWidth: .infinity)
 		.padding(20)
-		.contentShape(
-			Rectangle()
-		)
-		.buttonStyle(.plain)
+		.background(Color.pink.opacity(0.1))
+		.contentShape(Rectangle())
+		.onTapGesture {
+			print("작동")
+		}
     }
 }
 
